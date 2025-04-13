1473094d516efdf6d98baeb469b0d499f416602e
Albenus Pieter <146658653+albenusmurmu@users.noreply.github.com>
2025-04-13 18:40:24 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,14 +7,14 @@ const port = 8080;
 app.use(express.urlencoded({extended:true})); // middleware
 app.use(express.json()); // recieve data from server side in json format like -> hopscotch, postman, Thunder client.
 
-// It show user password and id into rul
+// It show user password and id into url(query)
 app.get("/register",(req,res) =>{
     let {user,password} = req.query;
     res.send(`standard get response. Welcome ${user}!`);
     
 });
 
-// It hide user password and id into rul and post it into the body
+// It show into the body 
 app.post("/register",(req,res) =>{
     let {user,password} = req.body;
     res.send(`standard post response. welcome ${user}`);
