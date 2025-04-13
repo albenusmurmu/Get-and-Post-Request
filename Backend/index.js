78673530e0750d6bf3fa43788efcfb5ea6143727
Albenus Pieter <146658653+albenusmurmu@users.noreply.github.com>
2025-04-13 18:31:36 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = 8080;
 
-// this handle the error from req.body -> why -> because req.bodey must know that which types of data should ntering
+// this handle the error from req.body -> why -> because req.bodey must know that which types of data should entering
 app.use(express.urlencoded({extended:true})); // middleware
 app.use(express.json()); // recieve data from server side in json format like -> hopscotch, postman, Thunder client.
 
