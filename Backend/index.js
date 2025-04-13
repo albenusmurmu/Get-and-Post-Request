cb323cc32ae853d25f5fb69948008ecb34f87dc1
Albenus Pieter <146658653+albenusmurmu@users.noreply.github.com>
2025-04-13 18:37:16 +0530

    Add commit

--- Backend/index.js
+++ Backend/index.js
@@ -3,17 +3,18 @@ const app = express();
 
 const port = 8080;
 
-// this handle the error from req.body -> why -> because req.bodey must know that which types of data should entering
+// This handle the error from req.body -> why -> because req.bodey must know that which types of data should entering
 app.use(express.urlencoded({extended:true})); // middleware
 app.use(express.json()); // recieve data from server side in json format like -> hopscotch, postman, Thunder client.
 
+// It show user password and id into rul
 app.get("/register",(req,res) =>{
     let {user,password} = req.query;
     res.send(`standard get response. Welcome ${user}!`);
     
 });
 
-
+// It hide user password and id into rul and post it into the body
 app.post("/register",(req,res) =>{
     let {user,password} = req.body;
     res.send(`standard post response. welcome ${user}`);
